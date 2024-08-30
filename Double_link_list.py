573b366ba511206d82716923d6e93bb16f417ef2
ikramulhaq63 <haq99831@gmail.com>
2024-08-30 16:27:35 +0500

    Implementation of Double link list using python

---
+++ Double_link_list.py
@@ -0,0 +1,76 @@
+class Node:
+    def __init__(self, prev= None,item=None,next=None):
+        self.prev = prev
+        self.item = item
+        self.next = next
+class DLL:
+    def __init__(self,start=None):
+        self.start=start
+    def _is_empty(self):
+        return self.start==None
+    def insert_at_start(self,data):
+         n=Node(None,data,self.start)
+         if not self._is_empty():
+            self.start.prev = n
+            self.start = n
+            self.start = n
+    def insert_at_last(self,data):
+        temp = self.start
+        if self.start != None:
+            while temp.next != None:
+                temp=temp.next
+            
+            n=Node(temp,data,None)
+        if temp == None:
+            self.start = None
+        else:
+            temp.next = n
+    def search(self,data):
+        temp=self.start
+        while temp is not None:
+            if temp.item == data:
+                return temp
+            temp=temp.next
+        return None
+    def insert_after(self,temp,data):
+        if temp is not None:
+            n = Node(temp,data,temp.next)
+            if temp.next is not None:
+                temp.next.prev = n
+                temp.next = n
+    def print_list(self):
+        temp = self.start
+        while temp is not None:
+            print(temp.item, end=" ")
+            temp=temp.next 
+    def delete_first(self):
+        if self.start is not None:
+            self.start=self.start.next
+            if self.start is not None:
+                self.start.prev = None
+    def delete_last(self):
+        if self.start is None:
+            pass
+        elif self.start.next is None:
+            self.start = None
+        else:
+            temp = self.start
+            while temp.next is not None:
+                temp = temp.next 
+                temp.prev.next = None
+    def delete_item(self,data):
+        if self.start is None:
+            pass
+        else:
+            temp = self.start
+            while temp is not None:
+                if temp.item == data:
+                    if temp.next is not None:
+                            temp.next.prev = temp.prev
+                    if temp.prev is not None:
+                        temp.prev.next= temp.next
+                    else:
+                        self.start=temp.next
+                    break
+                temp=temp.next
+
